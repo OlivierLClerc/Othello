f028582968086ed2e3773978bd2dbb3435811475
elie <elie.poncet92@gmail.com>
2023-03-17 12:26:48 +0100

    modif de engine et player

--- player.py
+++ player.py
@@ -7,7 +7,7 @@ class Joueur:
         x = input(f'C\'est au joueur {self.couleur}, entrez la position du coup : ')
         ligne_index = int(x[0])
         col_index = int(x[1])
-        while board.get_value(ligne_index, col_index) != " ":
+        while board.grid([ligne_index][col_index]) != " ":
             print('Case déjà occupée, veuillez choisir une autre position')
             x = input('Entrez la position du coup : ')
             ligne_index = int(x[0])
