37dc237355a18781e1319eca7c604a12397debfd
olivier_clerc <olivier.clerc@le-campus-numerique.fr>
2023-03-17 13:40:45 +0100

    pawn modified no reference to joueur in it

--- Pawn.py
+++ Pawn.py
@@ -1,40 +1,17 @@
 # Here the pawn/cell scr
 
-#class Pawn:
-#    def __init__(self, ligne_jeton, colonne_jeton, color):
-#        self.ligne_jeton = ligne_jeton
-#        self.colonne_jeton = colonne_jeton
-#        self.value = color
-#    
-#    def change_value(self, ligne_grid, colonne_grid):
-#        self.value = Board.grid[ligne_grid][colonne_grid]
-#        
-#    def cell_color(self):
-#        if self.value==1:
-#            print("white")
-#        elif self.value==-1:
-#            print("black")
-#        else:
-#            print("empty")
-# 
-#
-#
-#
-#        
-#
-#
-
 from player import Joueur
 
 class Pawn:
-    def __init__(self, form):
+    def __init__(self, couleur, form=" "):
+        self.couleur = couleur
         self.form = form
 
     
-    def pawn_form(self, form):
-        if Joueur.couleur == "black":
+    def pawn_form(self):
+        if self.couleur == "black":
             self.form = "X"
-        elif Joueur.couleur == "white":
+        elif self.couleur == "white":
             self.form = "O"
         else:
             self.form = " "
